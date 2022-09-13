8aa0446a6e2d4d3b632da2c2783d6c22f8d850b3
pnkmem421 <pnkmem421@gmail.com>
2022-09-13 17:59:31 +0900

    코드 정리

--- ble_for_cycle_test/lib/main.dart
+++ ble_for_cycle_test/lib/main.dart
@@ -24,25 +24,16 @@ class ESPTest extends StatefulWidget {
 }
 
 class _ESPTestState extends State<ESPTest> {
+  // 타겟 디바이스 해당 보드의 값 -> 해당 CYCLE_TEST만을 연결하기 위해 필요
   final String SERVICE_UUID =           "82e5f314-f248-4c9d-b802-385aa7fcaf24";
-  final String CHARACTERISTIC_UUID_RX = "b203f809-4542-47d2-a3b3-b662074bd171";
-  final String CHARACTERISTIC_UUID_TX = "25c48cde-1b14-4975-b95a-ebe47cf76aec";
   final String TARGET_DEVICE_NAME =     "CYCLE_TEST";
 
   var cycleModel = CycleModel(total: 0, speed: 0).obs; // for safety
 
+  // ESPController의 역할
   void espInit() {
-    //cardioModel.value = CardioModel(total: 0, speed: 0, level: 0);
     cycleModel.value = CycleModel(total: 0, speed: 0);
-    //range.value = 0;
-    //weight.value = 0;
-    //face.value = 0.0;
   }
-
-  //EspController espController = Get.find<EspController>();
-
-  // 메시지를 보내기 위한 컨트롤러
-  TextEditingController msgController = TextEditingController();
 
   // Obtain an instance for FlutterBlue
   FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
@@ -51,22 +42,19 @@ class _ESPTestState extends State<ESPTest> {
   bool isConnected = false;
 
   late List<ScanResult> scanResult; // Bluetooth Device Scan List
-
   late BluetoothDevice targetDevice;
-  late BluetoothCharacteristic targetCharacteristicRx;
-  late BluetoothCharacteristic targetCharacteristicTx;
 
   String connectionText = "No connection";
   String receivedValue ="";
 
-  // Scan을 시작함. List에 넣기만 하는 함수.
+  // Scan을 시작함 List에 넣기만 하는 함수.
   Future<void> startScan() async {
     print("Start Scan!");
     setState(() {
       connectionText = "Start Scanning";
     });
 
-    // Start scanning 3초간 진행됨.
+    // 3초간 스캔 진행함
     flutterBlue.startScan(timeout: const Duration(seconds:3), allowDuplicates: false);
 
     // Scan이 시작되는 동안에 받는 listener 관련 액션을 수행함.
@@ -77,20 +65,19 @@ class _ESPTestState extends State<ESPTest> {
       scanResult = data;});
     // Stop scanning
     flutterBlue.stopScan();
-    Future.delayed(const Duration(seconds: 5) , (){print("5초기다림ㅋ Listen 끝!"); scanDevice();});
+    Future.delayed(const Duration(seconds: 5) , (){scanDevice();});
 
   }
 
-  // Scan List에 있는 Device들 중, 내가 원하는 Device를 찾는 함수
+  // 검색된 Device 중 지정해둔 Device 검색
   Future<void> scanDevice() async {
     for (ScanResult r in scanResult) {
-      //print('${r.device.name} detected! / rssi: ${r.rssi}');
-      if (r.device.name == TARGET_DEVICE_NAME)
-      {
-        print("Target : ${r.device.name} Found!");
+      // 이 Device가 앞서 지정해둔 "CYCLE_TEST"와 같은지
+      if (r.device.name == TARGET_DEVICE_NAME) {
+        // 연결되었다면
         if (!isConnected) {
+          // 해당 Device 정보를 변수 targetDevice에 저장
           targetDevice = r.device;
-          print("*****************Target Device Info*******************\nTarget Device:$targetDevice");
           // Device Connect를 여기서 함.
           await connectToDevice(targetDevice);
         }
@@ -98,9 +85,9 @@ class _ESPTestState extends State<ESPTest> {
     }
   }
 
-  // 디바이스 연결을 위한 함수
+  // 디바이스 연결
   Future<void> connectToDevice(BluetoothDevice device) async {
-    print('*******************.....CONNECTING.....*********************');
+    print('*****.....CONNECTING.....*****');
     setState(() {
       connectionText = "Connect To Device";
     });
@@ -110,17 +97,17 @@ class _ESPTestState extends State<ESPTest> {
       await device.connect();
       isConnected = true;
       isDevice = true;
-      print('*********************DEVICE CONNECTED*********************');
+      print('*****DEVICE CONNECTED*****');
       await discoverServices(device);
     }
   }
 
+  // 연결을 끊는 부분
   disconnectFromDevice() {
     if (targetDevice == null) {
       print("No Device");
       return;
     }
-    targetCharacteristicTx.setNotifyValue(false);
     targetDevice.disconnect();
     flutterBlue = FlutterBluePlus.instance;
 
@@ -131,58 +118,37 @@ class _ESPTestState extends State<ESPTest> {
     });
   }
 
-  // 필요한 값들(서비스, 캐릭터리스틱 등)을 셋팅해줌
+  // 필요한 값들(서비스, 캐릭터리스틱 등)을 세팅
   Future<void> discoverServices(BluetoothDevice device) async {
     if (device == null) return;
 
-    print("******************DISCOVER SERVICES**************************");
+    print("*****DISCOVER SERVICES*****");
 
-    // 서비스 리스트 중 내 기기에 맞는걸? 찾아야함.
+    // 서비스 리스트 중 내 기기에 맞는 것을 찾아야함.
     List<BluetoothService> services = await device.discoverServices();
     services.forEach((service) {
       if (service.uuid.toString() == SERVICE_UUID) {
         service.characteristics.forEach((characteristic) {
           print(characteristic.uuid);
-          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID_RX) {
-            print("RX 감지덕지");
-            targetCharacteristicRx = characteristic;
-          }
-          else if (characteristic.uuid.toString() == CHARACTERISTIC_UUID_TX) {
-            print("TX 감지");
-            targetCharacteristicTx = characteristic;
 
-            characteristic.setNotifyValue(true);
+          characteristic.setNotifyValue(true);
 
-            /*
-            characteristic.value.listen((value) async{
-              cycleModel.value = CycleModel.fromString(String.fromCharCodes(value)); // discover_services
-              setState(() {});
-            });
-             */
-
-            characteristic.value.listen((value) {
-              if (value.length >0) {
-                receivedValue=value[0].toString();
-                cycleModel.value = CycleModel.fromString(String.fromCharCodes(value)); // discover_services
-                print("**[$receivedValue]**");
-              }
-              setState(() {});
-            });
-
-
-          }
+          characteristic.value.listen((value) {
+            if (value.length >0) {
+              receivedValue=value[0].toString(); // 캐리터리스틱을 통해 얻어온 값들을 가져오고
+              // 1. value값을 String.fromCharCodes을 통해 정수형 배열을 문자열로 파싱
+              // 2. CycleModel.fromString으로 total:0.0/speed:0.0을 변수 total과 speed에 각각 저장
+              cycleModel.value = CycleModel.fromString(String.fromCharCodes(value));
+              print("**[$receivedValue]**");
+            }
+            setState(() {}); // UI State에서 변경 사항이 있음을 Flutter Framework에 알려주는 역할을 함.UI에 변경된 값이 반영되도록 build 메소드가 다시 실행 UI 에 변경된 값이 반영될 수 있도록 build 메소드가 다시 실행
+          });
         });
       }
     });
   }
 
-  writeData(String data) async {
-    if (targetCharacteristicRx == null) return;
-
-    List<int> bytes = utf8.encode(data);
-    await targetCharacteristicRx.write(bytes);
-  }
-
+  // 화면 구성 내용
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -211,41 +177,14 @@ class _ESPTestState extends State<ESPTest> {
                   : const Text('[No Device Info]', style: TextStyle(color: Colors.purple,fontSize: 20.0, fontWeight: FontWeight.bold))
             ],
           ),
-          Expanded(child: Text("${receivedValue}", style: const TextStyle(color: Colors.red,fontSize: 20.0, fontWeight: FontWeight.bold))),
-          sendMessageArea()
         ],
       ),
     );
   }
-
-
-  // 메시지 전송을 위한 위젯
-  Widget sendMessageArea() {
-    return Card(
-      child: ListTile(
-        title: TextField(
-          controller: msgController,
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.send),
-          color: Colors.blue,
-          disabledColor: Colors.grey,
-          onPressed: (isConnected) ? sendMessage : null,
-        ),
-      ),
-    );
-  }
-
-  // 메시지 전송 처리
-  void sendMessage() {
-    writeData(msgController.text);
-    msgController.clear();
-  }
 }
 
-/// 유산소 모델 (보드에서 주는 정보)
+/// Cycle 모델 (CYCLE_TEST에서 주는 정보를 받아올 값)
 class CycleModel {
-  // total:69.30/speed:6.20
   double total; // 총 운동 거리(km)
   double speed; // 현재 속도(m/s)
 
@@ -255,6 +194,7 @@ class CycleModel {
   });
 
   factory CycleModel.fromString(String msg) {
+    // split : 일치하는 부분에서 문자열을 분할하고 pattern 하위 문자열 목록을 반환
     List<String> splitMsg = msg.split("/");
     return CycleModel(
       total: double.parse(splitMsg[0].split(":")[1]),
@@ -262,6 +202,7 @@ class CycleModel {
     );
   }
 
+  //String으로 변환하여 값을 내보냄
   @override
   String toString() {
     return
@@ -269,4 +210,3 @@ class CycleModel {
 ''';
   }
 }
-
